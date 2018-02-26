40c22d804319fdcef8baf9a1c631c91f4f598457
lucienbertin <me@lucienbertin.com>
2018-02-26 14:04:20 +0100

    add function to extract intervals between occurences of same tuple

---
+++ vigenere/utils/frequency.js
@@ -0,0 +1,22 @@
+const _ = require('underscore');
+
+function incompleteIntervals(txt, tuple) {
+	return _.chain(txt.split(tuple))
+	.rest()
+	.map(substr => `${tuple}${substr}`.length)
+	.value();
+}
+function intervals(txt, tuple) {
+	return _.chain(incompleteIntervals(txt, tuple))
+	.map((i, index, incompletes) => {
+		return _.chain(incompletes)
+		.rest(index)
+		.map((j, jndex, rest) => {
+			return _.chain(rest).first(jndex + 1).reduce((memo, interval) => memo + interval).value();
+		}).value();
+	})
+	.flatten()
+	.value();
+}
+
+module.exports.intervals = intervals;
